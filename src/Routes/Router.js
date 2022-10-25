46e40d44578a2a14e63d44b76717286c315c4a9f
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-26 00:22:03 +0600

    error page added

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../Components/Blog/Blog";
 import Courses from "../Components/Courses/Courses";
+import Error from "../Components/Error/Error";
 import Faq from "../Components/Faq/Faq";
 import Home from "../Components/Home/Home";
 import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 import Main from "../Layout/Main";
 
 export const routes = createBrowserRouter([
@@ -12,9 +14,10 @@ export const routes = createBrowserRouter([
             { path: '/', element: <Home /> },
             { path: '/courses', element: <Courses /> },
             { path: '/faq', element: <Faq /> },
-            { path: '/blog', element: <Blog></Blog> },
-            { path: '/login', element: <Login></Login> },
-            { path: '*', element: <h1>404 Not Found</h1> }
+            { path: '/blog', element: <Blog /> },
+            { path: '/login', element: <Login /> },
+            { path: '/register', element: <Register /> },
+            { path: '*', element: <Error /> }
         ]
     },
 ]);
